0f7d5d0d6b678e21f1a1245ab9aca182c260832b
Salome Kuitca <salokuitca@gmail.com>
2021-06-24 17:48:35 -0300

    Desafio1

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ContainerGeneral from './components/ContainerGeneral';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* Utilizo el componente ContainerGeneral para que se renderice */}
+      <ContainerGeneral></ContainerGeneral>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
